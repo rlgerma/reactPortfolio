019c8cde8dbaa423a791c8ad6c12d5c6a0a285b1
Richard Germaine <54249860+rlgerma92@users.noreply.github.com>
2021-06-03 11:50:44 -0600

    uupdates

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,8 +8,6 @@ exports.onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
     />,
   ]),
     setPreBodyComponents([
-      // <script src='node_modules/vanta/vendor/three.r95.min.js' />,
-      // <script src='node_modules/vanta/dist/vanta.clouds.min.js' />,
       <noscript key='noscript'>
         Your browser does not support JavaScript!
       </noscript>,
