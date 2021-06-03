efa71e4fc74fce4a46f3080c06cde40e23b0feb9
Richard Germaine <54249860+rlgerma92@users.noreply.github.com>
2021-06-03 10:29:54 -0600

    fixed major bug

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,16 @@
-const React = require('react');
+const React = require("react");
 
 exports.onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
   setHeadComponents([
     <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"
-      type="text/javascript"
+      src='https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js'
+      type='text/javascript'
     />,
   ]),
     setPreBodyComponents([
-      <script src="node_modules/vanta/vendor/three.r95.min.js" />,
-      <script src="node_modules/vanta/dist/vanta.clouds.min.js" />,
-      <noscript key="noscript">
+      // <script src='node_modules/vanta/vendor/three.r95.min.js' />,
+      // <script src='node_modules/vanta/dist/vanta.clouds.min.js' />,
+      <noscript key='noscript'>
         Your browser does not support JavaScript!
       </noscript>,
     ]);
